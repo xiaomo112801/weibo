375e0ef056c142091a4c1a56c9ca8913bd98d8d6
vagrant <vagrant@homestead>
2021-08-30 06:17:37 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,9 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     //
     public function create()
     {
@@ -38,5 +51,49 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+
+    public function update(User $user, Request $request)
+    {
+
+        $this->authorize('update', $user);
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['name'] = bcrypt($request->password);
+        }
+        DB::transaction(function () use ($data, $user) {
+            $user->update($data);
+
+        });
+
+        session()->flash('users.show', $user);
+//        Db::beginTransaction();
+//
+//        if ($user->update($data)) {
+//            Db::commit();
+//        } else {
+//            Db::rollBack();
+//        }
+//        $user = $user->update([
+//            'name' => $request->name,
+//            'password' => bcrypt($request->password),
+//        ]);
+
+        return redirect()->route('users.show', [$user]);
+
+    }
+
 
 }
